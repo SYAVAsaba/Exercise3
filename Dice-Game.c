68ed7cad0ce8fa1ce2bd77ac092cf53e3154fd4e
SYAVAsaba <40636541+SYAVAsaba@users.noreply.github.com>
2018-07-08 01:13:07 +0900

    Update Dice-Game.c
    Won/lost message modified

--- Dice-Game.c
+++ Dice-Game.c
@@ -22,7 +22,7 @@ int main() {
   printf("Die 1: %d\n",number1);
   printf("Die 2: %d\n",number2);
   printf("Total value: %d\n",sum);
-  if(sum>7) printf("You won!\n");
-  else printf("You lose!\n");
+  if(sum>7) printf("%s won!\n",name);
+  else printf("%s lose!\n",name);
   return 0;
 }
